a126cb045e99a3b2b0b930173e345c3f1eecc2c6
carry1224 <carrycathrine.gmail.com>
2024-07-26 11:56:38 +0300

    final commit

--- OneDrive/Desktop/first js project/app.js
+++ OneDrive/Desktop/first js project/app.js
@@ -1,3 +1,30 @@
+let value=document.getElementById('value')
+let btn=document.querySelectorAll('.btn')
+
+let initialValue=0
+btn.forEach(function(btns){
+  btns.addEventListener('click',function(e){
+    let styles=e.currentTarget.classList
+    if(styles.contains('increase')){
+      initialValue++
+    }else if(styles.contains('decrease')){
+      initialValue--
+    }else if(styles.contains('reset')){
+      initialValue=0
+    }
+    if(initialValue>0){
+      value.style.color='blue'
+    }
+    if(initialValue<0){
+      value.style.color='red'
+    }
+    if(initialValue===0){
+      value.style.color='yellow'
+    }
+    value.textContent=initialValue
+  })
+})
+
 
 
 
